aff6307ea481f53a0ea23a81c104d3d72e4d4be6
Jason Wall <javajo@gmail.com>
2014-12-29 15:16:25 -0700

    refactor into shared examples

--- spec/to_ascii/visitor_spec.rb
+++ spec/to_ascii/visitor_spec.rb
@@ -11,42 +11,49 @@ describe ToAscii::Visitor do
     col[1]
   end
 
-  subject { visitor }
+  shared_examples_for 'a column definer' do
+    describe :id do
+      it 'should, by default, add a column :id with width of 6' do
+        subject.id
+        expect(subject.columns.length).to eq 1
+        subject.columns.first.tap do |col|
+          expect(column_name_of col).to eq :id
+          expect(column_width_of col).to eq 6
+        end
+      end
 
-  describe :id do
-    it 'should, by default, add a column :id with width of 6' do
-      subject.id
-      expect(subject.columns.length).to eq 1
-      subject.columns.first.tap do |col|
-        expect(column_name_of col).to eq :id
-        expect(column_width_of col).to eq 6
+      it 'should accept a width param though' do
+        subject.id(18)
+        expect(column_width_of subject.columns.first).to eq 18
       end
     end
 
-    it 'should accept a width param though' do
-      subject.id(18)
-      expect(column_width_of subject.columns.first).to eq 18
+    describe :method_missing do
+      it 'should generate any column def' do
+        visitor.magic_potion
+        expect(subject.columns.length).to eq 1
+        subject.columns.first.tap do |col|
+          expect(column_name_of col).to eq :magic_potion
+          expect(column_width_of col).to eq 14
+        end
+      end
+
+      it 'should accept a width argument' do
+        subject.magic_potion(300)
+        expect(column_width_of subject.columns.first).to eq(300)
+      end
+
+      it 'should raise arity errors if has too many args' do
+        # this might save some people some headaches
+        expect { subject.magic(30, :id) }.to raise_error(ArgumentError, 'wrong number of arguments (2 for 0..1)')
+      end
     end
   end
 
-  describe :method_missing do
-    it 'should generate any column def' do
-      visitor.magic_potion
-      expect(subject.columns.length).to eq 1
-      subject.columns.first.tap do |col|
-        expect(column_name_of col).to eq :magic_potion
-        expect(column_width_of col).to eq 14
-      end
-    end
+  subject { visitor }
 
-    it 'should accept a width argument' do
-      subject.magic_potion(300)
-      expect(column_width_of subject.columns.first).to eq(300)
-    end
+  it_should_behave_like 'a column definer'
 
-    it 'should raise arity errors if has too many args' do
-      # this might save some people some headaches
-      expect { subject.magic(30, :id) }.to raise_error(ArgumentError, 'wrong number of arguments (2 for 0..1)')
-    end
+
   end
 end
