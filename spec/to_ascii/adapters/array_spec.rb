3820cce47ca95f8ef68446fd23a4bbfae3f84c42
Jason Wall <javajo@gmail.com>
2014-12-29 16:00:09 -0700

    change a couple specs to use new column definition form

--- spec/to_ascii/adapters/array_spec.rb
+++ spec/to_ascii/adapters/array_spec.rb
@@ -10,10 +10,7 @@ describe Array do
   let(:io) { StringIO.new }
 
   it 'should add to_ascii to an array' do
-    people.to_ascii(nil, io) do
-      column :first_name, 16
-      column :last_name, 16
-    end
+    people.to_ascii(nil, io) { columns first_name(16), last_name(16) }
 
     %w(Isaac Newton Albert Einstein first_name last_name).each do |word|
       expect(io.string).to match(/#{word}/)
